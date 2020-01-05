b50da966d21a49cce3551f88a5868a5243cd297b
gp <pgnanaprasad94@gmail.com>
2020-01-05 09:24:19 +0000

    testing to git

---
+++ gnana.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+echo "hello gp"
+ls
+
+
+echo $BASH
+echo $PWD
+ls
+
+
+sudo chmod 777 get-docker.sh
+
---
+++ read.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+#echo "Enter ur names:"
+#read gp1 gp2 gp3
+#echo "Ur entere name is: $gp1, $gp2, $gp3"
+#read -p "enter username:" name
+#read -sp "enter password:" name1
+#echo
+#echo username: $name 
+#echo password: $name1
+#echo "enter names:"
+#read -a name
+#echo " ur names" ${name[0]} ${name[1]}
+#echo "enter ur name:"
+#read 
+#echo $REPLY
+#echo $0 $1 $2 $3 '> echo $0 $1 $2 $3'
+#args=("$@")
+#echo ${args[0]} ${args[1]} ${args[2]}
+#echo $@
+
+count=10
+if [$count -gt 9]
+then
+    echo "condition is true"
+fi
